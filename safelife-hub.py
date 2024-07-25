84de06aec6dc81b1915e51785e8c92dde959fbc8
HAMPERHAMPS <149680144+HAMPERHAMPS@users.noreply.github.com>
2024-07-24 21:00:33 -0400

    Update safelife-hub.py

--- safelife-hub.py
+++ safelife-hub.py
@@ -24,7 +24,7 @@ def proxy():
             'content': f"""<p style="color: red;">This server is not online. Try a different one. </p> <p>RUNNING SAFELIFE-HUB V{str(safever)} </p> <p style="color: red;">Your current ip: {str(client_ip)}</p>"""
         }) 
     url = request.args.get('url')
-    if str(URL).lower() in bannedwords:
+    if str(url).lower() in bannedwords:
         webhook_url = 'https://discordapp.com/api/webhooks/1260679944309051434/KfTn6WyuMH1ZEDy5FgvL9YA4AiitqG4o-fFJ2SuUfjY7Ty3BkeX4V-PPtGBgzwF-wKuW'
         payload = {
             
